341024cb51f8e0e6dd6c9b0243580dcbe5f7c151
dev <sempernow.com@gmail.com>
2025-10-04 10:59:55 -0400

    ci-jobs.capture-pods.sh @ 2025-10-04T14:59Z

--- ci-jobs.capture-pods.sh
+++ ci-jobs.capture-pods.sh
@@ -4,7 +4,7 @@ NAMESPACE="${1:-glr-jobs}"
 manifests=${BASH_SOURCE%%*/}.yaml
 
 _ctrlC() {
-    echo -e "\n🔚 Manifests catpured to: $manifests"
+    echo -e "\n  🔚 Manifests catpured to: $manifests"
     exit 0
 }
 trap _ctrlC SIGINT # Call _ctrlC when CTRL+C is detected.
@@ -12,7 +12,7 @@ trap _ctrlC SIGINT # Call _ctrlC when CTRL+C is detected.
 kubectl get pods -n "$NAMESPACE" -w | while read line; do
     if [[ "$line" =~ "runner-" && "$line" =~ "Running" ]]; then
         POD_NAME=$(echo "$line" | awk '{print $1}')
-        echo " $(date -Is) ℹ️ Capture: $POD_NAME"
+        echo "  $(date -Is) ℹ️ Capture: $POD_NAME"
         echo '---' >> $manifests
         kubectl get pod "$POD_NAME" -n "$NAMESPACE" -o yaml >> $manifests
     fi
